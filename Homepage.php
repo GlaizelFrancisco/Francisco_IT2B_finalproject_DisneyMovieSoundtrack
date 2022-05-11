50d659563069dceaf29aba4cd547fd3674ac68c8
GlaizelFrancisco <polarismo25@gmail.com>
2022-05-11 16:05:55 +0800

    initial files and homepage done

---
+++ Homepage.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>DisneySoundtrack Homepage</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Disney</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+
+                        <li class="nav-item"><a class="nav-link text-white" class="btn btn-primary rounded submit p-3 px-5" href="Quote.php">Disney Quote Here</a></li>
+                </div>
+            </div>
+        </nav>
+
+
+
+        <!-- Header-->
+        <header
+            class=" text-muted ">
+            <div class="container px-4 text-center">
+
+            </div>
+
+<head>
+<style>
+h1 {text-align: center;
+    font-size: 50px;
+    font-style: italic;
+    font-weight: bold ;
+    color: whitesmoke;
+}
+
+h2  {text-align: center;
+    font-size: 20px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: gray;
+
+}
+
+
+    
+
+
+</style>
+</head>
+<body>
+<h1>Disney Movie Soundtrack</h1>
+<h2>-Adults are only kids grown up</h2>
+</html>
+<style type="text/css">
+    header{ background-color: #2F4F75;
+    }
+    body{ background: linear-gradient(to right, #CBBBD6, #9A9CC2);;
+     }
+
+</style>
+   
+</body>
+
+
+
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+</style>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+
+
+
+.row {
+  display: flex;
+}
+
+
+.column {
+  flex: 50%;
+  padding: 10px;
+  height: 600px; 
+}
+</style>
+</head>
+<body>
+
+
+<div class="row">
+  <div class="column" style="background-color:transparent;">
+    <h3>Disney Movie List</h3>
+    <p> <a href= "Encanto.php"
+    style="text-decoration: none; color:blueviolet; font-weight: bold;" >Encanto</a> </p>
+    <p> <a href= "Moana.php"style="text-decoration: none; color:blueviolet; font-weight: bold;">Moana</a> </p>
+    <p> <a href= "Frozen.php"style="text-decoration: none; color:blueviolet; font-weight: bold;">Frozen</a> </p>
+    <p> <a href= "Tangled.php"style="text-decoration: none; color:blueviolet; font-weight: bold;">Tangled</a> </p>
+    <p> <a href= "Tinkerbell.php"style="text-decoration: none; color:blueviolet; font-weight: bold;">Tinker Bell</a> </p>
+    <p> <a href= "Mulan.php"style="text-decoration: none; color:blueviolet; font-weight: bold;">Mulan</a> </p>
+    <p> <a href= "TheHunchback.php"style="text-decoration: none; color:blueviolet; font-weight: bold;">The Hunchback of Notre Dame</a> </p>
+    </div>
+  <div class="column" style="background-color:#transparent;">
+    <h3>Year Released</h3>
+    <p>2021</p>
+    <p>2016</p>
+    <p>2013</p>
+    <p>2010</p>
+    <p>2008</p>
+    <p>1998</p>
+    <p>1995</p>
+  </div>
+</div>
+
+</body>
+</html>
+
+
+        <?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+
+         ?>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Glaizel R. Francisco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
