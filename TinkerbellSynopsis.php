3f8ba9cfc2f1cec43abdd1a031bd14bb5231e8e3
GlaizelFrancisco <polarismo25@gmail.com>
2022-05-12 20:33:57 +0800

    Tinker Bell Synopsis

---
+++ TinkerbellSynopsis.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Tinker Bell Synopsis</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+
+
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <a href="Tinkerbell.php">
+            <img src="images/backicon.png" width="50" height="50"></a>
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Disney</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+
+                        <li class="nav-item"><a class="nav-link text-white" class="btn btn-primary rounded submit p-3 px-5" href="Homepage.php">Home</a></li>
+                </div>
+            </div>
+        </nav>
+
+
+
+        <!-- Header-->
+        <header
+            class=" text-muted ">
+            <div class="container px-4 text-center">
+
+            </div>
+
+<head>
+<style>
+h1 {text-align: center;
+    font-size: 50px;
+    font-style: italic;
+    font-weight: bold ;
+    color: whitesmoke;
+}
+
+h2  {text-align: center;
+    font-size: 20px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: black;
+
+}
+
+h3 { text-align: left;
+    font-size: 25px;
+    color: black;
+    padding-top: 10px;
+    font-weight: bold ;
+}
+
+
+    
+
+
+</style>
+</head>
+<body>
+<h1>Tinker Bell Movie Synopsis</h1>
+<h2>-"Your talent makes you who you are, you should be proud of it." </h2>
+</html>
+<style type="text/css">
+    header{ background-color: #758b42;
+    }
+    body{ background: linear-gradient(to right, #D79969, #F3D6A1);
+     }
+
+</style>
+   
+</body>
+
+
+
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-left">
+                    <div class="col-lg-12 text-left">
+
+</style>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+
+
+
+.row {
+  display: flex;
+}
+
+
+.column {
+  flex: 100%;
+  padding: 5px;
+  height: auto; 
+  border: 1px solid black;
+}
+
+</style>
+</head>
+<body>
+
+
+<div class="row">
+  <div class="column" style="background-color:transparent; height: auto;">
+    <img src="images/Synopsis/Tinkerbell.webp" style="padding-top: 10px ; padding-left: 10px;" width="500" height="250"></a> 
+    <a href= "https://www.bilibili.tv/en/video/2004315026?bstar_from=bstar-web.search-result.0.0"
+    style="text-decoration: none; color:blue; font-weight: bold; font-size: 30px; padding-left:  25px;" >Tinker Bell</a>
+    
+    <h3>Summaries</h3>
+    <?php
+    echo "<p> Enter the land of Tinker Bell and her four best fairy friends.</p>";
+     
+    echo "<p> Carried by the wind deep in the heart of the enchanting Pixie Hollow in the island of Neverland, the beautiful newly born fairy, Tinker Bell, discovers that her destiny as a tinker is to repair all sorts of things under the ever-watchful Queen Clarion. However, as the rebellious Tinker Bell yearns to fly along with her sisters, Iridessa, Silvermist, Rosetta, and Fawn to the mainland, she will soon realise that the arrival of spring requires special skills, hard work, and above all, faith. Will a dash of pixie dust help Tinker Bell stay true to herself?—Nick Riganas</p>";
+
+    
+    ?>
+
+ </div>
+
+ <div class="column" style="background-color:#FFFF00;">
+    <h3>Spoilers!</h3>
+    <?php
+    echo "<p> The synopsis below may give away important plot points.</p>";
+        ?>
+</div>
+  <div class="column" style="background-color:#transparent;">
+    <h3>Synopsis</h3>
+    <?php
+    echo "<p> Enter the magical world of fairies and meet the enchanting creatures of Pixie Hollow, who nurture nature and bring about the change of the seasons. Changing the colors of the leaves, moving a sunbeam to melt snow, waking animals from their winter slumber, or giving a patch of sproutlings a sprinkle of water are all within the realm of these seasonal specialists. Tinker Bell thinks her fairy talent as a tinker isn't as special or important as the other fairies' talents. But when Tink tries to change who she is, she creates nothing but disaster! With encouragement from her friends Rosetta, Silvermist, Fawn and Iridessa, Tink learns the key to solving her problems lies in her unique tinker abilities and discovers that when she's true to herself, magical things can happen.</p>";
+
+
+
+
+        ?>
+  
+
+
+  </div>
+</div>
+
+</body>
+</html>
+
+
+        <?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        
+
+         ?>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Glaizel R. Francisco</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
